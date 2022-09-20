10c1d5009de98884603110ca50ef29cf0911b5e0
Gumidafe Socorro <devgumidafe@gmail.com>
2022-09-20 20:28:02 +0100

    change logo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Adminpro
+# Admin Hospital
+
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/api'
+  //base_url: 'http://localhost:3000/api'
+  base_url: 'https://back-dashboard-hospital.herokuapp.com/api'
 };
 
